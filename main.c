e7a52a06c3c3ef81f7aaf338510a2780869d2a6c
Guillaume <guillaumexue@outlook.fr>
2023-03-30 18:51:40 +0200

    Les bases

--- main.c
+++ main.c
@@ -1,4 +1,24 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+struct noeud;
+struct liste_noeud;
+struct noeud {
+    bool est_dossier;
+    char nom[100];
+    struct noeud *pere;
+    struct noeud *racine;
+    struct liste_noeud *fils;
+};
+
+struct liste_noeud {
+    struct noeud *no;
+    struct liste_noeud *succ;
+};
+
+typedef struct noeud noeud;
+
+typedef struct liste_noeud liste_noeud;
 
 int main() {
     printf("Hello, World!\n");
