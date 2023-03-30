93d7e45102e7f0156965ad337d01404cda42ac5d
Guillaume <guillaumexue@outlook.fr>
2023-03-30 19:17:42 +0200

    ajout de fonction creer racine et de ls.

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 struct noeud;
 struct liste_noeud;
@@ -20,7 +21,29 @@ typedef struct noeud noeud;
 
 typedef struct liste_noeud liste_noeud;
 
+noeud creer_racine(){
+    noeud * res = malloc(sizeof (noeud));
+    res->est_dossier = true;
+    res->nom[0] = '\0';
+    res->pere = res;
+    res->racine = res;
+    res->fils = NULL;
+}
+
+void print_fils(liste_noeud *f){
+    printf("‰s\n",f->no->nom);
+    if(f != NULL){
+        print_fils(f->succ);
+    }
+}
+
+void ls(noeud *n){
+    if(n->fils != NULL){
+        print_fils(n->fils);
+    }
+}
+
 int main() {
-    printf("Hello, World!\n");
+
     return 0;
 }
