99478fd2c7eda637acf887bcbef3c9b8f17d1ec1
Guillaume <guillaumexue@outlook.fr>
2023-04-01 15:35:39 +0200

    Ajout fonction cd chem non tester

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <assert.h>
+#include <string.h>
 
 struct noeud;
 struct liste_noeud;
@@ -42,6 +44,45 @@ void ls(noeud *n){
     if(n->fils != NULL){
         print_fils(n->fils);
     }
+}
+
+int get_next_slash(int pos, const char * c){
+    int i = pos;
+    while (c[i] != '/' && c[i] !='\0'){
+        i++;
+    }
+    return i;
+}
+
+char * get_next_name_noeud(int fist, int last, const char * c){
+    char * tmp[strlen(c)];
+    for(int i=fist; i<=last; i++){
+        tmp[i] = (char *) c[i];
+    }
+    return tmp;
+}
+
+noeud * find_noeud(liste_noeud * l, const char * c){
+    assert(l == NULL);
+    if (strcmp(l->no->nom, c)){
+        return l->no;
+    }else{
+        find_noeud(l->succ, c);
+    }
+}
+
+noeud * cd_chem(noeud * n, const char * c){
+    assert(c[0] == '/');
+    noeud * res = n;
+    char * tmp;
+    int i = 0, j;
+    while (c[i] != '\0'){
+        j = i;
+        i = get_next_slash(j+1, c);
+        tmp = get_next_name_noeud(j, i, c);
+        res = find_noeud(res->fils,tmp);
+    }
+    return res;
 }
 
 int main() {
